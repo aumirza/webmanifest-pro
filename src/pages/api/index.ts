d5319675c4e3fd26d4d35e665f4dab8defa145e2
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2023-01-28 13:14:04 +0530

    fix : api, generating ,but not sending valid resp

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,7 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import formidable from "formidable";
-import { generateImages } from "../../helpers/ImageMaker";
+import {
+  cleanUp,
+  cropSquareImage,
+  generateImages,
+  getCrop,
+  makeFolders,
+  preCheck,
+} from "../../helpers/ImageMaker";
 import fs from "fs";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -9,6 +16,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     res.status(405).json({ message: "Method not allowed" });
     return;
   }
+
   const form = new formidable.IncomingForm({ multiples: true });
 
   form.parse(req, async (err, fields, files) => {
@@ -16,24 +24,35 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       console.error("Error", err);
       throw err;
     }
-    const { image } = files;
-    // create unique identifier for request
-    const uid =
-      Math.random().toString(36).substring(2, 15) +
-      Math.random().toString(36).substring(2, 15);
 
-    // generate images
+    const image = files.image as formidable.File;
+    const cropJsonString = fields.crop as string;
 
     try {
-      const zipPath = await generateImages(image, uid);
+      let crop = cropJsonString ? (JSON.parse(cropJsonString) as object) : null;
+      if (!crop) {
+        crop = await getCrop(image);
+      }
+      preCheck(image);
+      makeFolders(image.newFilename);
+      const newPath = await cropSquareImage(image, crop);
+      (image as formidable.File).filepath = newPath;
+      const zipPath = await generateImages(image);
+      cleanUp(image);
 
       if (!fs.existsSync(zipPath)) {
         throw new Error("Zip file not found");
       }
+
+      const stat = fs.statSync(zipPath);
       const zip = fs.readFileSync(zipPath);
 
+      res.setHeader(
+        "Content-disposition",
+        "attachment; filename=generated.zip"
+      );
       res.setHeader("Content-Type", "application/zip");
-      res.setHeader("Content-Length", zip.length);
+      res.setHeader("Content-Length", stat.size);
       res.setHeader("Content-Transfer-Encoding", "binary");
       res.setHeader("Expires", "0");
       res.setHeader(
@@ -43,8 +62,13 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       res.setHeader("Pragma", "public");
 
       res.status(200).send(zip);
+      // fs.unlinkSync(zipPath);
     } catch (error) {
-      res.status(500).json({ message: (error as any).message });
+      res.status(500).json({
+        message: (error as any).message
+          ? (error as any).message
+          : "Something went wrong",
+      });
     }
   });
 }
