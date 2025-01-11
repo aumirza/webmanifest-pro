4738b8f4e3d0bd25667822b07f1ec7f9178fd98e
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-01-11 12:10:28 +0530

    feat: enhance Footer component styles for better dark mode support and improve copyright display

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,11 +12,11 @@ export const Footer = () => {
         >
           <path
             d="M0 43.9999C106.667 43.9999 213.333 7.99994 320 7.99994C426.667 7.99994 533.333 43.9999 640 43.9999C746.667 43.9999 853.333 7.99994 960 7.99994C1066.67 7.99994 1173.33 43.9999 1280 43.9999C1386.67 43.9999 1440 19.0266 1440 9.01329V100H0V43.9999Z"
-            className="text-gray-500 fill-current"
+            className="text-gray-600 fill-current dark:text-gray-500"
           ></path>
         </svg>
       </div>
-      <div className="flex flex-col items-center flex-grow text-white bg-gray-500">
+      <div className="flex flex-col items-center flex-grow text-white bg-gray-600 dark:bg-gray-500">
         <div className="flex items-end justify-center flex-grow">
           <div className="flex gap-1">
             <span>Made with ❤️ by</span>
@@ -27,7 +27,8 @@ export const Footer = () => {
         </div>
         <div className="flex justify-between w-11/12 pb-5 text-sm max-w-7xl">
           <div className="">
-            <span>copyright 2025</span>
+            <span className="text-base">&copy;&nbsp;</span>
+            <span>Copyright 2025</span>
           </div>
           <ul className="flex gap-2">
             <li>
@@ -35,10 +36,11 @@ export const Footer = () => {
                 Terms of service
               </Link>
             </li>
-            <Link className="underline" href="/privacy">
-              Privacy and policy
-            </Link>
-            <li></li>
+            <li>
+              <Link className="underline" href="/privacy">
+                Privacy and policy
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
